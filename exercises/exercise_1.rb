c49f64e1d9889a0eaf0e29cabbaeb10b00a11b4d
Adam Yiu <adam_yiu@outlook.com>
2022-06-29 17:49:49 +0000

    Add 3 stores and count rows

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,16 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+puts ActiveRecord::Base.connection.tables
+
+
+# Example create
+# user = User.create(name: "David", occupation: "Code Artist")
+
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: 1000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
