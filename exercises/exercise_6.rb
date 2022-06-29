9ba0904ab3c32eede57d832e0706f523888770b2
Adam Yiu <adam_yiu@outlook.com>
2022-06-29 19:48:20 +0000

    Add validation to tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -18,10 +18,10 @@ class Employee
   belongs_to :store
 end
 
-@store1.employees.create(first_name: "Bob", last_name: "Ross", hourly_rate: 30)
-@store1.employees.create(first_name: "Margaret", last_name: "Thatcher", hourly_rate: 100000)
-@store1.employees.create(first_name: "Barack", last_name: "Obama", hourly_rate: 20000)
+@store1.employees.create(first_name: "Bob", last_name: "Ross", hourly_rate: 50)
+@store1.employees.create(first_name: "Margaret", last_name: "Thatcher", hourly_rate: 60)
+@store1.employees.create(first_name: "Barack", last_name: "Obama", hourly_rate: 70)
 
-@store2.employees.create(first_name: "Boris", last_name: "Johnson", hourly_rate: 90)
-@store2.employees.create(first_name: "Theresa", last_name: "May", hourly_rate: 500)
-@store2.employees.create(first_name: "Henry", last_name: "Ford", hourly_rate: 600)
+@store2.employees.create(first_name: "Boris", last_name: "Johnson", hourly_rate: 80)
+@store2.employees.create(first_name: "Theresa", last_name: "May", hourly_rate: 90)
+@store2.employees.create(first_name: "Henry", last_name: "Ford", hourly_rate: 100)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,23 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, :last_name, :store_id, presence: true
+  validates :hourly_rate, inclusion: 40..200
+end
+
+class Store
+  validates :store_name, :annual_revenue, :mens_apparel, :womens_apparel, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
+
+
+end
+
+puts "Please enter a store name"
+
+store_name = gets.chomp
+
+Store.create(name: "#{store_name}")
